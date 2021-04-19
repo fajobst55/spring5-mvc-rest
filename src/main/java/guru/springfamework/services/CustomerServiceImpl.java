30e7ce33c91353c34feada2c6e7b1f4b092e36dc
Frank Jobst <fajobst@gmail.com>
2021-04-19 17:57:42 -0500

    patch-customer-controller

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -81,7 +81,9 @@ public class CustomerServiceImpl implements CustomerService {
                 customer.setLastname(customerDTO.getLastname());
             }
 
-            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+            CustomerDTO returnDTO = customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+            returnDTO.setCustomerUrl("/api/v1/customer/"+id);
+            return returnDTO;
         }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
     }
 }
